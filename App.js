a52e594f39058b233a372531541a889b52fed324
Ghozi Izzulhaq <ghozihaq2017@gmail.com>
2024-06-22 00:45:25 +0700

    feat: add weather app

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+// Import axios, BASE_URL, dan API_KEY
+import axios from 'axios';
+import { BASE_URL, API_KEY } from './src/constants';
+import { View, StyleSheet, ActivityIndicator, Text } from 'react-native';
+import WeatherSearch from './src/components/weatherSearch';
+import WeatherInfo from './src/components/weatherInfo';
 
-export default function App() {
+const App = () => {
+  // Definisikan state "weatherData" dan "setWeatherData"
+  const [weatherData, setWeatherData] = useState();
+  const [status, setStatus] = useState('');
+
+  const renderComponent = () => {
+    switch (status) {
+      case 'loading':
+        return <ActivityIndicator size="large" />;
+      case 'success':
+        return <WeatherInfo weatherData={weatherData} />;
+      case 'error':
+        return <Text>Something went wrong. Please try again with a correct city name.</Text>;
+        default:
+          return null; 
+    }
+  };
+
+  const searchWeather = (location) => {
+    // Mengatur status ke "loading"
+    setStatus('loading')
+    axios
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+      .then((response) => {
+        const data = response.data
+        data.visibility /= 1000
+        data.visibility = data.visibility.toFixed(2)
+        data.main.temp -= 273.15
+        data.main.temp = data.main.temp.toFixed(2)
+        setWeatherData(data)
+        // Mengatur status ke "success"
+        setStatus('success')
+      })
+      .catch((error) => {
+        // Mengatur status ke "error"
+        setStatus('error')
+        console.log(error);
+      })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WeatherSearch searchWeather={searchWeather} />
+      {/* Menggunakan function renderComponent di sini */}
+      <View style={styles.margintTop20}>{renderComponent()}</View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    padding: 20
+  }
 });
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_KEY = '3300a7e160e53ca330c3879ba6b922de';
